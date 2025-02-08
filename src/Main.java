43a335eefd0adb36953d9babcd4169862dff0365
Andreu02 <sergio.andreu@edu.uah.es>
2025-02-08 11:37:44 +0100

    ent1.eja.e8 COMPLETADO

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,47 @@
 //Importamos la clase
-import ent1.eja.e7.Time;
+import ent1.eja.e8.Ball;
 
 
 public class Main {
     public static void main(String[] args) {
-        ent1_eja_e7();
+        ent1_eja_e8();
     }
-    static public void ent1_eja_e7() {
-        // Test constructors and toString()
-        Time t1 = new Time(1, 2, 3);
-        System.out.println(t1); // toString()
+    static public void ent1_eja_e8() {
+        // Test constructor and toString()
+        Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
+        System.out.println(ball); // toString()
 // Test Setters and Getters
-        t1.setHour(4);
-        t1.setMinute(5);
-        t1.setSecond(6);
-        System.out.println(t1); // toString()
-        System.out.println("Hour: " + t1.getHour());
-        System.out.println("Minute: " + t1.getMinute());
-        System.out.println("Second: " + t1.getSecond());
-// Test setTime()
-        t1.setTime(23, 59, 58);
-        System.out.println(t1); // toString()
-// Test nextSecond();
-        System.out.println(t1.nextSecond());
-        System.out.println(t1.nextSecond().nextSecond());
-// Test previousSecond()
-        System.out.println(t1.previousSecond());
-        System.out.println(t1.previousSecond().previousSecond());
+        ball.setX(80.0f);
+        ball.setY(35.0f);
+        ball.setRadius(5);
+        ball.setxDelta(4.0f);
+        ball.setyDelta(6.0f);
+        System.out.println(ball); // toString()
+        System.out.println("x is: " + ball.getX());
+        System.out.println("y is: " + ball.getY());
+        System.out.println("radius is: " + ball.getRadius());
+        System.out.println("xDelta is: " + ball.getxDelta());
+        System.out.println("yDelta is: " + ball.getyDelta());
+// Bounce the ball within the boundary
+        float xMin = 0.0f;
+        float xMax = 100.0f;
+        float yMin = 0.0f;
+        float yMax = 50.0f;
+        for (int i = 0; i < 15; i++) {
+            ball.move();
+            System.out.println(ball);
+            float xNew = ball.getX();
+            float yNew = ball.getY();
+            int radius = ball.getRadius();
+// Check boundary value to bounce back
+            if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
+                ball.reflectHorizontal();
+            }
+            if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
+                ball.reflectVertical();
+            }
+        }
+
     }
 }
 
